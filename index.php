71058e43174dcfbc64e29bf9657624d2415dc9d2
ChauveauAdrien <adrienpro.chauveau@gmail.com>
2022-02-22 13:14:12 +0100

    css

--- index.php
+++ index.php
@@ -5,8 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload Image</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    
+    <div class="upload-wrapper">
+        <h1>Veuillez choisir une image :</h1>
+        <img id="imgPreview">
+        <input type="file">
+        <button><ion-icon name="cloud-upload-outline"></ion-icon>UPLOAD</button>
+    </div>
+
+
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
